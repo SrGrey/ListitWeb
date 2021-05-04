496368244f327b4492fe0d7caf1d5d85a456e019
SrGrey <avargin.s@gmail.com>
2021-05-04 08:38:22 +0100

    SECRET_KEY modifyed

--- listit/settings.py
+++ listit/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'caxh1x!ybo#%o0^j8c1i0f_gwz-86wyy2tcb42f0y14+9x-9mt'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
